3fb0630faad4a8561737e3084f5f0427f7b72f07
Bob Fornal <bob.fornal@leadingedje.com>
2024-07-24 22:15:26 -0400

    Update index.js

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ const streamingInit = require('./streaming/index.js');
 streamingInit(app);
 
 app.listen(config.PORT, () => {
-  console.log(`Server running at http://localhost:${config.PORT || 10000}/`);
+  console.log(`Server running at http://localhost:${process.env.PORT || 10000}/`);
 });

2763947cf24e31d2f731717aed3711e0d2f18012
Bob Fornal <bob@code-squid.com>
2024-07-24 22:23:46 -0400

    Update Port

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ const app = express();
 const streamingInit = require('./streaming/index.js');
 streamingInit(app);
 
-app.listen(config.PORT, () => {
+app.listen(process.env.PORT, () => {
   console.log(`Server running at http://localhost:${process.env.PORT || 10000}/`);
 });

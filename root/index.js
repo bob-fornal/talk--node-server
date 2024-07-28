9952ddda722e493da610d72bb33fd4f998dba294
Bob Fornal <bob.fornal@leadingedje.com>
2024-07-28 13:50:15 -0400

    Fixing __dirname Issue

--- root/index.js
+++ root/index.js
@@ -1,4 +1,8 @@
-import { join } from 'path';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default function init(app) {
   app.get('/', (request, response) => {
